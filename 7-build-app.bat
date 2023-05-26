25765d4e7587adf81656ef384ceffd9cc574a34d
Ugur Coruh <ugur.coruh.tr@gmail.com>
2023-05-26 16:01:26 +0300

    paths are refactored

--- 7-build-app.bat
+++ 7-build-app.bat
@@ -10,9 +10,9 @@ echo Change the current working directory to the script directory
 @cd /d "%~dp0"
 
 echo Delete the "docs" folder and its contents
-rd /S /Q "calculator-app/target/site/coverxygen"
-rd /S /Q "calculator-app/target/site/coveragereport"
-rd /S /Q "calculator-app/target/site/doxygen"
+rd /S /Q "calculator-app\target\site\coverxygen"
+rd /S /Q "calculator-app\target\site\coveragereport"
+rd /S /Q "calculator-app\target\site\doxygen"
 
 echo Delete and Create the "release" folder and its contents
 rd /S /Q "release"
@@ -47,10 +47,10 @@ echo Change directory to calculator-app
 cd calculator-app
 
 echo Generate ReportGenerator HTML Report
-call reportgenerator "-reports:target/site/jacoco/jacoco.xml" "-sourcedirs:src/main/java" "-targetdir:target/site/coveragereport" -reporttypes:Html
+call reportgenerator "-reports:target\site\jacoco\jacoco.xml" "-sourcedirs:src\main\java" "-targetdir:target\site\coveragereport" -reporttypes:Html
 
 echo Generate ReportGenerator Badges
-call reportgenerator "-reports:target/site/jacoco/jacoco.xml" "-sourcedirs:src/main/java" "-targetdir:target/site/coveragereport" -reporttypes:Badges
+call reportgenerator "-reports:target\site\jacoco\jacoco.xml" "-sourcedirs:src\main\java" "-targetdir:target\site\coveragereport" -reporttypes:Badges
 
 echo Display information about the binary file
 echo Our Binary is a Single Jar With Dependencies. You Do Not Need to Compress It.
@@ -59,10 +59,10 @@ echo Return to the previous directory
 cd ..
 
 echo Run Coverxygen
-call python -m coverxygen --xml-dir ./calculator-app/target/site/doxygen/xml --src-dir ./ --format lcov --output ./calculator-app/target/site/coverxygen/lcov.info --prefix %currentDir%\calculator-app\
+call python -m coverxygen --xml-dir ./calculator-app/target/site/doxygen/xml --src-dir ./ --format lcov --output ./calculator-app/target/site/coverxygen/lcov.info --prefix %currentDir%/calculator-app/
 
 echo Run lcov genhtml
-call perl C:\ProgramData\chocolatey\lib\lcov\tools\bin\genhtml ./calculator-app\target\site\coverxygen\lcov.info --legend --title "Documentation Coverage Report" -o calculator-app/target/site/coverxygen
+call perl C:\ProgramData\chocolatey\lib\lcov\tools\bin\genhtml --legend --title "Documentation Coverage Report" ./calculator-app/target/site/coverxygen/lcov.info -o calculator-app/target/site/coverxygen
 
 echo Copy badge files to the "assets" directory
 call copy "calculator-app\target\site\coveragereport\badge_combined.svg" "assets\badge_combined.svg"
@@ -76,7 +76,7 @@ call copy "assets\rteu_logo.jpg" "calculator-app\src\site\resources\images\rteu_
 echo Copy the "assets" folder and its contents to "maven site images" recursively
 call robocopy assets "calculator-app\src\site\resources\assets" /E
 
-echo Copy the "README.md" file to "calculator-app/src/site/markdown/readme.md"
+echo Copy the "README.md" file to "calculator-app\src\site\markdown\readme.md"
 call copy README.md "calculator-app\src\site\markdown\readme.md"
 
 cd calculator-app
@@ -85,22 +85,22 @@ call mvn site
 cd ..
 
 echo Package Output Jar Files
-tar -czvf release/application-binary.tar.gz -C calculator-app/target '*.jar'
+tar -czvf release\application-binary.tar.gz -C calculator-app\target '*.jar'
 
 echo Package Jacoco Test Coverage Report (Optional)
-call tar -czvf release/test-jacoco-report.tar.gz -C calculator-app/target/site/jacoco .
+call tar -czvf release\test-jacoco-report.tar.gz -C calculator-app\target\site\jacoco .
 
 echo Package ReportGenerator Test Coverage Report
-call tar -czvf release/test-coverage-report.tar.gz -C calculator-app/target/site/coveragereport .
+call tar -czvf release\test-coverage-report.tar.gz -C calculator-app\target\site\coveragereport .
 
 echo Package Code Documentation
-call tar -czvf release/application-documentation.tar.gz -C calculator-app/target/site/doxygen .
+call tar -czvf release\application-documentation.tar.gz -C calculator-app\target\site\doxygen .
 
 echo Package Documentation Coverage
-call tar -czvf release/doc-coverage-report.tar.gz -C calculator-app/target/site/coverxygen .
+call tar -czvf release\doc-coverage-report.tar.gz -C calculator-app\target\site\coverxygen .
 
 echo Package Product Site
-call tar -czvf release/application-site.tar.gz -C calculator-app/target/site .
+call tar -czvf release\application-site.tar.gz -C calculator-app\target\site .
 
 echo ....................
 echo Operation Completed!
